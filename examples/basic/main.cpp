e3eb2c5cfbfa4b3b51234964251658fca1ddf25f
becem-gharbi <becem.gharbi@live.com>
2023-10-10 15:45:34 +0100

    chore: Update basic example

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAdmin.h>
+#include <ArduinoJson.h>
 #include "credentials.h"
 
 ESPAdmin::Logger logger("app");
+
+void onCustomCommand(String message);
 
 void setup()
 {
@@ -21,7 +24,14 @@ void setup()
 
     logger.success("WiFi connected");
 
-    ESPAdmin::begin(httpHost, deviceId, httpCert, mqttCert);
+    ESPAdmin::begin(httpHost, deviceId, apiKey, httpCert, mqttCert);
+
+    ESPAdmin::Command::onCustom = &onCustomCommand;
+}
+
+void onCustomCommand(String message)
+{
+    logger.info("Handling command " + message);
 }
 
 void loop()
