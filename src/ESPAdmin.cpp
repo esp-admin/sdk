1d9a41a511dc67055dd91c066315b73f92b9e9be
becem-gharbi <becem.gharbi@live.com>
2024-05-23 10:57:48 +0100

    refactor: increase log message max size

--- src/ESPAdmin.cpp
+++ src/ESPAdmin.cpp
@@ -7,7 +7,7 @@ namespace ESPAdmin
     options.resetDelayMs = DEFAULT_INT(options.resetDelayMs, 7000);
     options.httpMaxResponseSize = DEFAULT_INT(options.httpMaxResponseSize, 300);
     options.httpTimeoutMs = DEFAULT_INT(options.httpTimeoutMs, 8000);
-    options.logMaxMessageSize = DEFAULT_INT(options.logMaxMessageSize, 100);
+    options.logMaxMessageSize = DEFAULT_INT(options.logMaxMessageSize, 200);
     options.mqttTaskPriority = DEFAULT_INT(options.mqttTaskPriority, 5);
     options.mqttTaskStackSize = DEFAULT_INT(options.mqttTaskStackSize, 6896); // StackHighWaterMark = 4848 bytes
     options.mqttKeepAliveSec = DEFAULT_INT(options.mqttKeepAliveSec, 30);
