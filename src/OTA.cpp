7c4763f71189b18220de226682989cdfddd4ae16
becem-gharbi <becem.gharbi@live.com>
2024-06-19 08:24:13 +0100

    refactor(OTA): use advanced APIs

--- src/OTA.cpp
+++ src/OTA.cpp
@@ -13,29 +13,61 @@ namespace ESPAdmin
 
     void OTA::task(void *)
     {
-        esp_http_client_config_t config = {
+
+        esp_http_client_config_t httpConfig = {
             .url = _downloadURL.c_str(),
             .cert_pem = Store::options.httpCert,
             .timeout_ms = Store::options.httpTimeoutMs,
         };
 
-        Update::onChange(UPDATE_STARTED);
+        esp_https_ota_config_t otaConfig = {
+            .http_config = &httpConfig,
+        };
 
         Store::updateRunning = true;
+        Update::onChange(UPDATE_STARTED);
 
-        esp_err_t ret = esp_https_ota(&config);
+        esp_https_ota_handle_t otaHandle;
 
-        if (ret == ESP_OK)
-        {
-            Store::updateRunning = false;
-            Update::onChange(UPDATE_SUCCEDED);
-        }
-        else
+        esp_err_t ret = esp_https_ota_begin(&otaConfig, &otaHandle);
+
+        if (ret != ESP_OK)
         {
             Store::updateRunning = false;
             Update::onChange(UPDATE_FAILED);
         }
 
+        while (Store::updateRunning)
+        {
+            esp_err_t ret = esp_https_ota_perform(otaHandle);
+
+            if (ret == ESP_ERR_HTTPS_OTA_IN_PROGRESS)
+            {
+                continue;
+            }
+            else if (ret == ESP_OK)
+            {
+                Store::updateRunning = false;
+
+                bool completed = esp_https_ota_is_complete_data_received(otaHandle);
+
+                if (completed)
+                {
+                    Update::onChange(UPDATE_SUCCEDED);
+                }
+                else
+                {
+                    Update::onChange(UPDATE_FAILED);
+                }
+            }
+            else
+            {
+                Store::updateRunning = false;
+                Update::onChange(UPDATE_FAILED);
+            }
+        }
+
+        esp_https_ota_finish(otaHandle);
         vTaskDelete(nullptr);
     }
 }
