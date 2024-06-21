2c8cffa653fbfdbadc675d6d5ba529d3f1da2a1c
becem-gharbi <becem.gharbi@live.com>
2024-06-21 18:46:28 +0100

    chore: change OTA and MQTT default stack size

--- src/ESPAdmin.cpp
+++ src/ESPAdmin.cpp
@@ -16,12 +16,12 @@ namespace ESPAdmin
     options.httpTimeoutMs = DEFAULT_INT(options.httpTimeoutMs, 8000);
     options.logMaxMessageSize = DEFAULT_INT(options.logMaxMessageSize, 200);
     options.mqttTaskPriority = DEFAULT_INT(options.mqttTaskPriority, 5);
-    options.mqttTaskStackSize = DEFAULT_INT(options.mqttTaskStackSize, 6896); // StackHighWaterMark = 4848 bytes
+    options.mqttTaskStackSize = DEFAULT_INT(options.mqttTaskStackSize, 9640); // Stack used 4820 bytes
     options.mqttKeepAliveSec = DEFAULT_INT(options.mqttKeepAliveSec, 30);
     options.mqttNetworkTimeoutMs = DEFAULT_INT(options.mqttNetworkTimeoutMs, 8000);
     options.mqttReconnectTimeoutMs = DEFAULT_INT(options.mqttReconnectTimeoutMs, 10000);
     options.otaTaskPriority = DEFAULT_INT(options.otaTaskPriority, 5);
-    options.otaTaskStackSize = DEFAULT_INT(options.otaTaskStackSize, 6896); // StackHighWaterMark = 3384 bytes
+    options.otaTaskStackSize = DEFAULT_INT(options.otaTaskStackSize, 6928); // Stack used 3464 bytes
 
     Store::begin(options);
 
